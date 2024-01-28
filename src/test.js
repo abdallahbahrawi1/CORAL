600f74fdf868561836e8e67a2aac03cef67a29f7
fadihalteh <141834276+fadihalteh@users.noreply.github.com>
2024-01-28 17:29:23 +0200

    Delete src/test.js

--- src/test.js
+++
@@ -1,7 +0,0 @@
-function generateRandomString() {
-  const randomNumber = Math.floor(Math.random() * 1000000000);
-  const randomString = `#${String(randomNumber).padStart(9, '0')}`;
-  return randomString;
-}
-
-console.log(generateRandomString())
